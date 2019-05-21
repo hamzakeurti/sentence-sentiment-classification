ceb4ad633d72883706d7e9a255825e2440a15705
Hamza Keurti <hamza.keurti@gmail.com>
2019-05-21 16:01:03 +0800

    possibility to log to files

--- codes/utils.py
+++ codes/utils.py
@@ -1,7 +1,23 @@
 from datetime import datetime
+import logging
+
+logs_dir = 'logs/'
+log_file = ''
+
+def INIT_LOG(log=''):
+    global log_file
+    if not log:
+        return
+    log_file = logs_dir + log 
+    logging.basicConfig(filename=log_file,level=logging.INFO)
+
 
 
 def LOG_INFO(msg):
     now = datetime.now()
     display_now = str(now).split(' ')[1][:-3]
-    print('[' + display_now + ']' + ' ' + msg)
+    if not log_file:
+        print('[' + display_now + ']' + ' ' + msg)
+    else:
+        logging.info('[' + display_now + ']' + ' ' + msg)
+
